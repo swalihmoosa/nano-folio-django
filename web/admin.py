ba53c14a11ae70656ad94cdc952a64ba34679cdb
swalihmoosa <swalihmoosa72@gmail.com>
2021-12-04 15:51:34 +0530

    Gallery and Category model created

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from web.models import Category, Gallery
+
+
+admin.site.register(Category)
+
+
+admin.site.register(Gallery)
--- web/models.py
+++ web/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=155)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["id"]
+
+
+class Gallery(models.Model):
+    image = models.ImageField(upload_to="gallery/")
+
+    def __str__(self):
+        return self.image
+
+    class Meta:
+        ordering = ["id"]
+
+
